7489875fb133833b6fcfb872454da747a947536d
Franco Serio <franco.serio@rocketinsights.com>
2021-07-01 11:43:47 -0300

    Add CSRF Protection

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fastifyCsrf from 'fastify-csrf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.enableCors();
+  app.register(fastifyCsrf);
   await app.listen(3000);
 }
 bootstrap();
