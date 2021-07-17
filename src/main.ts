7f4fd03bb8c359d7a6ab2401e98ad2da70055836
Franco Serio <francoagustinserio@gmail.com>
2021-07-17 00:13:26 -0300

    Add fastify logger

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ logger: true }),
   );
   app.enableCors();
   await app.listen(3000);
