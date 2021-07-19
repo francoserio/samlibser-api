72b0088c87e639040f05d4903d972d48eb42f165
Franco Serio <francoagustinserio@gmail.com>
2021-07-19 10:37:54 -0300

    Change env variables and remove console.log

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -7,9 +7,9 @@ import { UsersService } from 'src/users/users.service';
 export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private readonly userService: UsersService) {
     super({
-      clientID: process.env['OAUTH_GOOGLE_ID'],
-      clientSecret: process.env['OAUTH_GOOGLE_SECRET'],
-      callbackUrl: process.env['OAUTH_GOOGLE_REDIRECT_URL'],
+      clientID: process.env.OAUTH_GOOGLE_ID,
+      clientSecret: process.env.OAUTH_GOOGLE_SECRET,
+      callbackUrl: process.env.OAUTH_GOOGLE_REDIRECT_URL,
       scope: ['email', 'profile'],
     });
   }
@@ -22,7 +22,6 @@ export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
     const { name, email } = profile._json;
     const [firstName, lastName] = name.split(' ');
 
-    console.log('eeee');
     let user = this.userService.findOne(email);
     if (!user) {
       user = this.userService.create({
